389c5aaafc4b9bdc7f2b456738017507113172b6
sroessner <sroessner@users.noreply.github.com>
2015-10-06 15:21:52 -0400

    Update Scripts.js

--- Scripts.js
+++ Scripts.js
@@ -54,7 +54,6 @@ function dropeitem(ev) {
      copy but to alter and move elements. By appending a ".cloneNode(true)", 
      you will not move the original element, but create a copy. */
   if (el.id/el.id!=NaN){el=el.parentNode;}
-  alert("eitem"+el.id);
   var nodeCopy = document.getElementById(data).cloneNode(true);
   nodeCopy.id = name; /* We cannot use the same ID */
   el.appendChild(nodeCopy);
