69b084caa61b2ee7d8fa33ee1a5bd739387ccfd8
sroessner <sroessner@users.noreply.github.com>
2015-10-06 14:54:17 -0400

    Update Scripts.js

--- Scripts.js
+++ Scripts.js
@@ -52,7 +52,7 @@ function dropeitem(ev) {
   /* If you use DOM manipulation functions, their default behaviour it not to 
      copy but to alter and move elements. By appending a ".cloneNode(true)", 
      you will not move the original element, but create a copy. */
-  if (el.id!="canvas"){el=el.parentNode;}
+  if (el.id/el.id!="NaN"){el=el.parentNode;}
   var nodeCopy = document.getElementById(data).cloneNode(true);
   nodeCopy.id = name; /* We cannot use the same ID */
   el.appendChild(nodeCopy);
